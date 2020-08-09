f93ee51d6d61fe3922e48002234513bc8ba5922a
Zhenyu,Liu <liuzhenyulive@live.com>
2020-08-09 22:39:32 +0800

    remove icon from bootstrap

--- src/main.js
+++ src/main.js
@@ -7,16 +7,13 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue'
 import 'bootswatch/dist/litera/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@/styles/custom.css'
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
-Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
 
